c02e131b34fd9c039d006181bbb6653a4cebb348
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-08-06 22:23:25 +0530

    Add files via upload

---
+++ pocketMoney.py
@@ -0,0 +1,7 @@
+pocketMoney=int(input("ENter the amount of pocket money you get:"))
+if(pocketMoney>500):
+    print("You are a rich kid")
+elif(pocketMoney>100):
+    print("ypu are comfortable")
+else:
+    print("I know how how feel")
