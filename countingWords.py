cbfabb15e58dad4bb2615028e544ea46d097e1d1
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-08-06 22:22:56 +0530

    Update countingWords.py

--- countingWords.py
+++ countingWords.py
@@ -3,6 +3,7 @@ characterCount=0
 wordCount=1
 for i in introString:
       characterCount=characterCount+1
+      #mandatory to give space on betwen the ' '
       if(i==' '):
             wordCount=wordCount+1
 print("Number of word in the string:")
